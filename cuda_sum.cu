6c0b0c5604031c32053c49f0237eafc7f27aaf22
hnagata <h0.n.xdg@gmail.com>
2015-02-02 16:38:13 +0900

    Optimize cuda_sum

--- cuda_sum.cu
+++ cuda_sum.cu
@@ -3,28 +3,32 @@
 #include "perform.h"
 #include "cuda_common.h"
 
-const int BLOCK_SIZE = 256;
+const int GRID_SIZE = 448;
+const int BLOCK_SIZE = 32;
 
-__global__ void sum_dev(double *a, size_t n, double *b) {
+__global__ void sum_dev(double *a, int n, double *b) {
+	__shared__ double work[BLOCK_SIZE];
+	const int tidx = threadIdx.x;
+	const int bidx = blockIdx.x;
 	const int idx = threadIdx.x + blockIdx.x * blockDim.x;
 	const int dim = blockDim.x * gridDim.x;
-	extern __shared__ double tmp[];
+
+	work[tidx] = 0;
+
 	for (int i = idx; i < n; i += dim) {
-		tmp[threadIdx.x] += a[i];
+		work[tidx] += a[i];
 	}
 	__syncthreads();
 
-	int w = blockDim.x;
-	while (w > 8) {
+	int w = BLOCK_SIZE;
+	while (w > 2) {
 		w /= 2;
-		if (threadIdx.x < w) tmp[threadIdx.x] += tmp[threadIdx.x + w];
+		if (tidx < w) work[tidx] = work[tidx + w];
 		__syncthreads();
 	}
 
-	if (threadIdx.x == 0) {
-		double s = 0;
-		for (int i = 0; i < w; i++) s += tmp[i];
-		b[blockIdx.x] = s;
+	if (tidx == 0) {
+		b[bidx] = work[0] + work[1];
 	}
 }
 
@@ -34,24 +38,22 @@ bool prepare() {
 }
 
 double sum(const std::vector<double> &a) {
-	int bsize = BLOCK_SIZE;
-	int gsize = (a.size() + bsize - 1) / bsize;
-
 	double *a_dev = NULL, *b_dev = NULL;
-	double ans;
+	double *b = new double[GRID_SIZE];
+	double ans = 0;	
 	CUDA_CALL(cudaMalloc((void**) &a_dev, sizeof(double) * a.size()));
-	CUDA_CALL(cudaMalloc((void**) &b_dev, sizeof(double) * gsize));
 	CUDA_CALL(cudaMemcpy(
 			a_dev, a.data(), sizeof(double) * a.size(), 
 			cudaMemcpyHostToDevice));
-	sum_dev<<<gsize, bsize, bsize * sizeof(double)>>>(a_dev, a.size(), b_dev);
-	CUDA_CHECK();
-	sum_dev<<<1, bsize, bsize * sizeof(double)>>>(b_dev, gsize, b_dev);
+	CUDA_CALL(cudaMalloc((void**) &b_dev, sizeof(double) * GRID_SIZE));
+	sum_dev<<<GRID_SIZE, BLOCK_SIZE>>>(a_dev, a.size(), b_dev);
 	CUDA_CHECK();
 	CUDA_CALL(cudaMemcpy(
-			&ans, b_dev, sizeof(double), cudaMemcpyDeviceToHost));
+			b, b_dev, sizeof(double) * GRID_SIZE, cudaMemcpyDeviceToHost));
+	for (int i = 0; i < GRID_SIZE; i++) ans += b[i];
 finally:
 	cudaFree(a_dev);
 	cudaFree(b_dev);
+	delete[] b;
 	return ans;
 }
